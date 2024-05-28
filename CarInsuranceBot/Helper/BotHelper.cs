96463af1074a91d146dc4fa7cb709cf5a30027ae
Illia Voloshyn <voloshin.ilya.andreevich@gmail.com>
2024-05-28 15:35:46 +0300

    Added more interaction with OpenAI

--- CarInsuranceBot/Helper/BotHelper.cs
+++ CarInsuranceBot/Helper/BotHelper.cs
@@ -35,7 +35,7 @@ namespace CarInsuranceBot.Helper
                 if (update.Message is not { } message)
                     return;
 
-                var customerId = update.Message.From!.Id;
+                var customerId = message.From!.Id;
                 await _customerService.InitializeCustomerIfItNotExistAsync(customerId);
 
                 if (message.Text is not null && message.Text.StartsWith("/"))
@@ -45,7 +45,6 @@ namespace CarInsuranceBot.Helper
                 };
 
                 var userStage = await _customerService.GetCustomerCurrentStageAsync(customerId);
-
                 await HandleUserMessageAsync(message, userStage, cancellationToken, customerId);
             }
             catch (Exception ex)
@@ -75,7 +74,7 @@ namespace CarInsuranceBot.Helper
         {
             return message.Text switch
             {
-                "/start" => HandleDialogStartAsync(message.Text, message.Chat.Id, cancellationToken, customerId),
+                "/start" => HandleDialogStartAsync(message, cancellationToken, customerId),
                 "/help" => _botClient.SendTextMessageAsync(
                     chatId: message.Chat.Id,
                     text:
@@ -106,21 +105,22 @@ namespace CarInsuranceBot.Helper
         {
             return (userStage switch
             {
-                Stage.Greeting when message.Text is not null => HandleDialogStartAsync(message!.Text, message.Chat.Id, cancellationToken, customerId),
+                Stage.Greeting when message.Text is not null => HandleDialogStartAsync(message, cancellationToken, customerId),
                 Stage.Passport when message.Photo is not null => HandlePassportUploadingAsync(message!, customerId, cancellationToken),
                 Stage.PassportConfirmation when message.Text is not null => HandlePassportCoinfirmationAsync(message!.Text, message.Chat.Id, cancellationToken, customerId),
                 Stage.Title when message.Photo is not null => HandleTitleUploadingAsync(message!, customerId, cancellationToken),
                 Stage.TitleConfirmation when message.Text is not null => HandleTitleCoinfirmationAsync(message!.Text, message.Chat.Id, cancellationToken, customerId),
                 Stage.PriceQuotation when message.Text is not null => HandlePriceQuotationAsync(message!.Text, message.Chat.Id, cancellationToken, customerId),
+                Stage.Finish => HandleFinishStateAsync(message!.Chat.Id, cancellationToken),
                 _ => HandleUnexpectedCaseAsync(message.Chat.Id, cancellationToken)
             });
         }
 
-        private async Task HandleDialogStartAsync(string messageText, long chatId, CancellationToken cancellationToken, long customerId)
+        private async Task HandleDialogStartAsync(Message message, CancellationToken cancellationToken, long customerId)
         {
-            _logger.LogInformation($"Bot started a conversation in chat: {chatId}");
+            _logger.LogInformation($"Bot started a conversation in chat: {message.Chat.Id}");
             string botResponse;
-            if (string.Equals(messageText, "/start", StringComparison.OrdinalIgnoreCase))
+            if (string.Equals(message.Text, "/start", StringComparison.OrdinalIgnoreCase))
             {
                 botResponse = await GetReplyFromOpenAIAsync(
                     """
@@ -139,20 +139,19 @@ namespace CarInsuranceBot.Helper
             }
            
             await _botClient.SendTextMessageAsync(
-                chatId: chatId,
+                chatId: message.Chat.Id,
                 text: botResponse,
                 cancellationToken: cancellationToken);
         }
 
         private async Task HandleTitleUploadingAsync(Message message, long id, CancellationToken cancellationToken)
         {
-            string responseMessage = string.Empty;
             try
             {
                 var base64Image = await GetBase64EncodedPhotoAsync(message);
                 var result = await _mindeeService.ParseTitleImageAsync(base64Image);
-                responseMessage = result is null 
-                    ? responseMessage 
+                string responseMessage = result is null 
+                    ? string.Empty 
                     : result.ToString();
                 var customerData = await _customerService.GetCustomerByIdAsync(id);
                 customerData.UpdateCustomersTitleDataAndStage(result!, Stage.TitleConfirmation);
@@ -206,7 +205,13 @@ namespace CarInsuranceBot.Helper
 
         private async Task HandleTitleCoinfirmationAsync(string message, long chatId, CancellationToken cancellationToken, long id)
         {
-            if (string.Equals(message, "yes", StringComparison.OrdinalIgnoreCase))
+            var openAIRerly = await GetReplyFromOpenAIBasedOnUserMessageAsync(
+                """
+                The user has answered whether their document data is correct.
+                Based on their response, decide if it is positive or negative. 
+                If positive, reply with 'yes'; if negative, reply with 'no'; if you have concers answer 'idk'
+                """, message);
+            if (string.Equals(openAIRerly, "yes", StringComparison.OrdinalIgnoreCase))
             {
                 await Task.WhenAll(_botClient.SendTextMessageAsync(
                     chatId: chatId,
@@ -218,7 +223,7 @@ namespace CarInsuranceBot.Helper
                     cancellationToken: cancellationToken),
                 _customerService.SetCustomerStageAsync(id, Stage.PriceQuotation));
             }
-            else if (string.Equals(message, "no", StringComparison.OrdinalIgnoreCase))
+            else if (string.Equals(openAIRerly, "no", StringComparison.OrdinalIgnoreCase))
             {
                 await Task.WhenAll(_botClient.SendTextMessageAsync(
                     chatId: chatId,
@@ -230,14 +235,20 @@ namespace CarInsuranceBot.Helper
             {
                 await _botClient.SendTextMessageAsync(
                     chatId: chatId,
-                    text: "Unexpected message, please type \"yes\" or \"no\" to continue",
+                    text: await GetReplyFromOpenAIAsync("Tell the user that you didn't understand and ask him to express their thoughts more clearly."),
                     cancellationToken: cancellationToken);
             }
         }
 
         private async Task HandlePassportCoinfirmationAsync(string message, long chatId, CancellationToken cancellationToken, long customerId)
         {
-            if (string.Equals(message, "yes", StringComparison.OrdinalIgnoreCase))
+            var openAIRerly = await GetReplyFromOpenAIBasedOnUserMessageAsync(
+                """
+                The user has answered whether their document data is correct.
+                Based on their response, decide if it is positive or negative. 
+                If positive, reply with 'yes'; if negative, reply with 'no'; if you have concers answer 'idk'
+                """, message);
+            if (string.Equals(openAIRerly, "yes", StringComparison.OrdinalIgnoreCase))
             {
                 await Task.WhenAll(_botClient.SendTextMessageAsync(
                     chatId: chatId,
@@ -245,7 +256,7 @@ namespace CarInsuranceBot.Helper
                     cancellationToken: cancellationToken),
                 _customerService.SetCustomerStageAsync(customerId, Stage.Title));
             }
-            else if (string.Equals(message, "no", StringComparison.OrdinalIgnoreCase))
+            else if (string.Equals(openAIRerly, "no", StringComparison.OrdinalIgnoreCase))
             {
                 await Task.WhenAll(_botClient.SendTextMessageAsync(
                     chatId: chatId,
@@ -257,47 +268,30 @@ namespace CarInsuranceBot.Helper
             {
                 await  _botClient.SendTextMessageAsync(
                     chatId: chatId,
-                    text: "Unexpected message, please type \"yes\" or \"no\" to continue",
+                    text: await GetReplyFromOpenAIAsync("Tell the user that you didn't understand and ask him to express their thoughts more clearly."),
                     cancellationToken: cancellationToken);
             }
         }
 
         private async Task HandlePriceQuotationAsync(string message, long chatId, CancellationToken cancellationToken, long customerId)
         {
-            if (string.Equals(message, "yes", StringComparison.OrdinalIgnoreCase))
+            var openAIRerly = await GetReplyFromOpenAIBasedOnUserMessageAsync(
+                """
+                User was replyed on the proposition of car insurance,
+                Make a decision is this reply positive or negative,
+                if positive answer 'yes' if negative 'no' if you havе concerns answer 'idk'
+                """, message);
+            if (string.Equals(openAIRerly, "yes", StringComparison.OrdinalIgnoreCase))
             {
                 var customerData = await _customerService.GetCustomerByIdAsync(customerId);
-                var result = await _openAIClient.GetChatCompletionsAsync(new ChatCompletionsOptions
-                {
-                    Messages =
-                    {
-                        new ChatRequestSystemMessage(
-                                """
-                                Genereate message in the following format and replase stars by value from message :
-                                Wonderfull,
-                                Your Car Insurance
-                                ----------------------------
-                                Policy Number: *******
-                                Full Name: *******
-                                Passport Type and Number: *****
-                                VechileIdentificationNumber = *******
-                                ----------------------------
-                                After that say thank you for choossing our service and mention that the conversion is over
-                                """),
-                        new ChatRequestUserMessage($"Generate a dummy car insurance policy document with the following data:\nPolicy Number: 123456789\nFull Name:{customerData.FullName}\nVechileIdentificationNumber: {customerData.VechileIdentificationNumber}\nPassport Type and Number: {customerData.PassTypeAndNumber}\nPrice: 100 USD")
-                    },
-                    MaxTokens = 150, 
-                    DeploymentName = "gpt-3.5-turbo",
-                    ChoiceCount = 1
-                });
-
+                var dummyCarInsurance = await GetDummyInsuranceFromOpenAI(customerData);
                 await Task.WhenAll(_botClient.SendTextMessageAsync(
                     chatId: chatId,
-                    text: result.Value.Choices[0].Message.Content,
+                    text: dummyCarInsurance,
                     cancellationToken: cancellationToken),
                     _customerService.SetCustomerStageAsync(customerId, Stage.Finish));
             }
-            else if (string.Equals(message, "no", StringComparison.OrdinalIgnoreCase))
+            else if (string.Equals(openAIRerly, "no", StringComparison.OrdinalIgnoreCase))
             {
                 var openAIResponse = await GetReplyFromOpenAIAsync(
                     """
@@ -314,11 +308,18 @@ namespace CarInsuranceBot.Helper
             {
                 await _botClient.SendTextMessageAsync(
                     chatId: chatId,
-                    text: "Unexpected message, please type \"yes\" or \"no\" to continue",
+                    text: await GetReplyFromOpenAIAsync("Tell the user that you didn't understand and ask him to express their thoughts more clearly."),
                     cancellationToken: cancellationToken);
             }
         }
 
+        private Task HandleFinishStateAsync(long chatId, CancellationToken cancellationToken)
+        {
+            return _botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "The conversation with bot is over try to use bot's command if you want to start is again",
+                cancellationToken: cancellationToken);
+        }
         private Task HandleUnexpectedCaseAsync(long chatId, CancellationToken cancellationToken)
         {
             return _botClient.SendTextMessageAsync(
@@ -343,6 +344,66 @@ namespace CarInsuranceBot.Helper
                     tell the user to try repeate a litle bit later or contact with support
                     """
                 );
+        }
+
+        private async Task<string> GetReplyFromOpenAIBasedOnUserMessageAsync(string systemMessage, string userMessage)
+        {
+            try
+            {
+                var openAIResponse = await _openAIClient.GetChatCompletionsAsync(new ChatCompletionsOptions
+                {
+                    Messages = 
+                    { 
+                        new ChatRequestSystemMessage(systemMessage),
+                        new ChatRequestUserMessage(userMessage)
+                    },
+                    MaxTokens = 130,
+                    DeploymentName = "gpt-3.5-turbo",
+                    ChoiceCount = 1
+                });
+                return openAIResponse.Value.Choices[0].Message.Content;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error during OpenAI API Request :{ex.GetType().Name} {ex.Message}");
+                return "Some of our service is currently unavaliable, please contact with customer support";
+            }
+        }
+
+        private async Task<string> GetDummyInsuranceFromOpenAI(Customer customerData)
+        {
+            try
+            {
+                var result = await _openAIClient.GetChatCompletionsAsync(new ChatCompletionsOptions
+                {
+                    Messages =
+                    {
+                        new ChatRequestSystemMessage(
+                                """
+                                Genereate message in the following format and replase stars by value from message :
+                                Wonderfull,
+                                Your Car Insurance
+                                ----------------------------
+                                Policy Number: *******
+                                Full Name: *******
+                                Passport Type and Number: *****
+                                VechileIdentificationNumber = *******
+                                ----------------------------
+                                After that say thank you for choossing our service and mention that the conversion is over
+                                """),
+                        new ChatRequestUserMessage($"Generate a dummy car insurance policy document with the following data:\nPolicy Number: 123456789\nFull Name:{customerData.FullName}\nVechileIdentificationNumber: {customerData.VechileIdentificationNumber}\nPassport Type and Number: {customerData.PassTypeAndNumber}\nPrice: 100 USD")
+                    },
+                    MaxTokens = 150,
+                    DeploymentName = "gpt-3.5-turbo",
+                    ChoiceCount = 1
+                });
+                return result.Value.Choices[0].Message.Content;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error during OpenAI API Request :{ex.GetType().Name} {ex.Message}");
+                return "Some of our service is currently unavaliable, please contact with customer support";
+            }
         }
 
         private async Task<string> GetReplyFromOpenAIAsync(string systemMessage)
